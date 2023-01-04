e28b8e46f33794df5e062b0f5026a4ad8993677b
Matthias Wieser <matthias.wieser.83@live.de>
2023-01-04 08:08:48 +0100

    fix: 0002 - sort companies by title

---
+++ src/controller/companiesController.js
@@ -0,0 +1,97 @@
+import { Company } from "../models/Company.js";
+
+// ###################################################################################################
+// ######################################## BASIC CRUD ###############################################
+// ###################################################################################################
+
+// ########################################## Create #################################################
+// ----------------------------------------- new company ------------------------------------------------
+export const createCompany = async (req, res) => {
+	const { company } = req.body;
+	const { associationId } = req.params;
+	company.associationId = associationId;
+	try {
+		const createdCompany = await Company.create(company);
+		res.status(201).json({
+			company: createdCompany,
+			message: "Company was created successfully",
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "Creation of the new company failed!" });
+	}
+};
+
+// ######################################## Read #####################################################
+// ----------------------------------------- All -----------------------------------------------------
+export const getAllCompanies = async (req, res) => {
+	try {
+		const companies = await Company.find().sort("title");
+		res.status(200).json({ companies: companies });
+	} catch (error) {
+		res.status(500).json({ error: "Companys request failed" });
+	}
+};
+
+export const getAllCompaniesByAssociationId = async (req, res) => {
+	const { associationId } = req.params;
+	console.log(associationId);
+	try {
+		let companies = await Company.find({
+			associationId: associationId,
+		});
+		if (companies) {
+			companies.sort((a, b) => {
+				if (a.title < b.title) return -1;
+				if (a.title > b.title) return 1;
+				return 0;
+			});
+			res.status(200).json({ companies: companies });
+		} else {
+			res.status(404).json({
+				message: `No companies for associationId ${associationId} found!`,
+			});
+		}
+	} catch (error) {
+		res.status(500).json({ error: "Companies request failed" });
+	}
+};
+
+// ------------------------------------------- Single By ID ------------------------------------------
+export const getSingleCompanyByCompanyId = async (req, res) => {
+	const { companyId } = req.params;
+	try {
+		const company = await Company.findById(companyId);
+		res.status(200).json({ company: company });
+	} catch (error) {
+		res.status(500).json({ error: "Company request failed" });
+	}
+};
+
+// ######################################## Update ###################################################
+// -------------------------------------- Update company ------------------------------------------------
+export const updateCompany = async (req, res) => {
+	const { companyId } = req.params;
+	const { company } = req.body;
+	try {
+		Company.modifiedAt = new Date();
+		const resCompany = await Company.findByIdAndUpdate(companyId, company, {
+			new: true,
+		});
+		res.status(200).json({ company: resCompany });
+	} catch (error) {
+		res.status(500).json({ error: "Modifying of the company failed!" });
+		console.log(error);
+	}
+};
+
+// ######################################## Delete ###################################################
+export const deleteCompany = async (req, res) => {
+	const { companyId } = req.params;
+	try {
+		await company.findByIdAndDelete(companyId);
+		res.status(200).json({ message: "Company successfully deleted!" });
+	} catch (error) {
+		res.status(500).json({ error: "Deleting of the Company failed!" });
+	}
+};
